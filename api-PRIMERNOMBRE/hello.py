f3fc543a3abd8997a320dcf2dd2713b662ad587f
Braulio Guevara <65747744+BraulioGuevara@users.noreply.github.com>
2024-02-21 16:40:45 -0600

    Add files via upload

--- api-PRIMERNOMBRE/hello.py
+++ api-PRIMERNOMBRE/hello.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('mongodb://mongodb.db.svc.cluster.local:27017/')
+db = client['mydatabase']
+collection = db['users']
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users = [user['name'] for user in collection.find({}, {'_id': 0, 'name': 1})]
+    return jsonify(users)
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    name = data.get('name')
+    if name:
+        user = {'name': name}
+        collection.insert_one(user)
+        return 'Usuario creado exitosamente', 201
+    else:
+        return 'Error: Se requiere un nombre para crear un usuario', 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+
+
